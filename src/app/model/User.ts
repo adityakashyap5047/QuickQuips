4fbf9869a83f851ccc30992632757644dc68a13f
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-07-21 00:36:28 +0530

    creating the user schema

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -17,3 +17,53 @@ const MessageSchema: Schema<Message> = new Schema({
     }
 });
 
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptinMessage: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+\@.+\..+/, "Please fill a valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptinMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages: [MessageSchema]
+});
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
