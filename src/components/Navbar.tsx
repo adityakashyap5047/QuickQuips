3b134111571fc4bf98f6b9385455aa6ca511b640
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-07-22 02:22:34 +0530

    completing the message card

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className='p-4 md:p-6 shadow-md'>
         <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-            <a className='text-xl font-bold mb-4 md:mb-0' href="#">QuickQuips</a>
+            <a className='text-xl font-bold mb-4 md:mb-0' href="/">QuickQuips</a>
             {
                 session ? (
                     <>
