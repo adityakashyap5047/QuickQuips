a42d1c1c301934441c6e183ab2e73eff4e0fd1ec
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-07-21 16:38:21 +0530

    wrapping the app from the session provider

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
-function page() {
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>page</div>
+    <>
+      Not signed in <br />
+      <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default page
--- src/middlware.ts
+++ src/middlware.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
+
 export {default} from 'next-auth/middleware'
 import { getToken } from 'next-auth/jwt'
  
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
 
     const token = await getToken({req: request})
@@ -18,11 +18,13 @@ export async function middleware(request: NextRequest) {
         ) {
             return NextResponse.redirect(new URL('/dashboard', request.url))
     }
+    if(!token && url.pathname.startsWith('/dashboard')) {
+      return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
 
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.next()
 }
  
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     '/sign-in',
